9aab06666fc11725273c9d98b06a00427ae14c00
Gabriel Camargo <gcamargo.dev@gmail.com>
2024-02-18 16:00:00 -0300

    feat: change max width to timeline component

--- src/components/Timeline/TimelineRoot.tsx
+++ src/components/Timeline/TimelineRoot.tsx
@@ -4,7 +4,7 @@ interface ITimelineRootProps {
 
 export function TimelineRoot({ children }: ITimelineRootProps) {
   return (
-    <div className="flex flex-col max-w-[512px] max-md:w-full max-md:max-w-full  mx-auto">
+    <div className="flex flex-col max-w-[768px] max-md:w-full max-md:max-w-full  mx-auto">
       <div className="flex h-full gap-4">
         <span className="w-[1px] bg-white opacity-75 h-[full]"></span>
         {children}
