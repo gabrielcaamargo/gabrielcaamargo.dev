f8362dcd3d7e09d263e8bc6a3ce4913c143f3eb3
Gabriel Camargo <gcamargo.dev@gmail.com>
2024-02-18 16:33:30 -0300

    feat: create another square on timeline item

--- src/components/Timeline/TimelineContent.tsx
+++ src/components/Timeline/TimelineContent.tsx
@@ -12,7 +12,8 @@ interface ITimelineContentProps extends ComponentProps<'div'> {
 export function TimelineContent({ company, workingTime, role, description, technologies, className }: ITimelineContentProps) {
   return (
     <div className={`relative ${className}`}>
-      <span className="absolute top-0 left-0 h-3 w-3 rounded bg-white opacity-75 ml-[-22px] mt-1"></span>
+      <span className="absolute top-0 left-0 h-3 w-3 rounded bg-white opacity-65 ml-[-22px] mt-1"></span>
+      <span className="absolute top-1 left-1 h-3 w-3 rounded bg-white opacity-85 ml-[-22px] mt-1"></span>
       <strong className="block text-lg">{company}</strong>
       <span className="block font-medium opacity-95">{workingTime}</span>
       <span className="block opacity-75">{role}</span>
@@ -20,7 +21,7 @@ export function TimelineContent({ company, workingTime, role, description, techn
         <p>{description}</p>
         <div className="flex mt-2">
           <span className="font-medium">Technologies:</span>
-          <div className="ml-2 grid grid-cols-4 gap-1">
+          <div className="ml-2 grid grid-cols-4 gap-1 max-md:grid-cols-2">
             {technologies.map((technology, index) => <Badge title={technology} key={index} />)}
           </div>
         </div>
